3d35ddd1c6c8a1747cdb3402080a808ead6acdc5
jeni <143943786+Zhenya228555@users.noreply.github.com>
2025-06-21 19:06:37 +0300

    Update hesu.py

--- eqs-main/eqs-main/wdqe-main/wdqe-main/wwwe-main/JENIPLAY/iuqqgpiqijqif/server/hesu.py
+++ eqs-main/eqs-main/wdqe-main/wdqe-main/wwwe-main/JENIPLAY/iuqqgpiqijqif/server/hesu.py
@@ -1,27 +1,37 @@
+#підключеня модуля
 import socket 
-# import pygame
-
+from threading import Thread
+#дае змогу пидключеня клийента до сервера
 desoll=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 desoll.bind(("localhost",1337))
 desoll.setblocking(False)
 desoll.listen(5)
 plaers = {}
 id = 1
+
+
+def a():
+    for connect in list(plaers):
+
+        try:
+            e=connect.recv(1024).decode()
+            print(e)
+        except:
+            pass
+
+
+#говорить шо вин працюе
 print("working")
+#список клийентиф
 clients = []
+#бескинечний цикел
 while True:
     try:
         connect, ip = desoll.accept()
         connect.setblocking(False)
-#     print("dlabla", ip)
-        plaers[connect] = {
-            "id":id,
-            "x":0,
-            "y":0,
-            "radius":40,
-            "name": None
-        }
-        connect.send(f"{plaers["id"]},{plaers["x"]},{plaers["y"]},{plaers["radius"]}{plaers["name"]}".encode())
+        plaers[connect] = {"id":id,"x":0,"y":0,"radius":40,"name": None}
+        connect.send(f"{id},0,0,20".encode())
         id += 1
+        
     except:
         pass 
